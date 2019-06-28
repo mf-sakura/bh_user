6db379d68d6a5267de27819fadec0341ef30905e
mf-sakura <sakura.yuto@moneyforward.co.jp>
2019-06-28 15:27:37 +0900

    fix

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -10,6 +10,11 @@ import (
 	"github.com/grpc-ecosystem/go-grpc-middleware/tracing/opentracing"
 	"google.golang.org/grpc"
 	"net"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	open_config "github.com/uber/jaeger-client-go/config"
+	"time"
 )
 
 const (
@@ -18,6 +23,26 @@ const (
 
 func main() {
 	fmt.Println("Process Started.")
+	cfg := open_config.Configuration{
+		Sampler: &open_config.SamplerConfig{
+			Type:  "const",
+			Param: 1,
+		},
+		Reporter: &open_config.ReporterConfig{
+			LogSpans:            true,
+			BufferFlushInterval: 1 * time.Second,
+			LocalAgentHostPort:  "jaeger:6831",
+		},
+	}
+	tracer, closer, err := cfg.New(
+		"booking_hotel",
+		open_config.Logger(jaeger.StdLogger),
+	)
+	defer closer.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	opentracing.SetGlobalTracer(tracer)
 	conf, err := config.LoadConifg()
 	if err != nil {
 		panic(err)
