127689ba135b6a5b8497e3c49fe619b157bc687f
mf-sakura <sakura.yuto@moneyforward.co.jp>
2019-06-28 14:37:43 +0900

    add opentracing

--- app/server/user.go
+++ app/server/user.go
@@ -6,6 +6,8 @@ import (
 	hpb "github.com/mf-sakura/bh_user/app/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+
+	"github.com/opentracing/opentracing-go"
 )
 
 type UserServiceServerImpl struct {
@@ -19,12 +21,16 @@ func (u *UserServiceServerImpl) RegistUser(ctx context.Context, req *hpb.RegistU
 	return nil, nil
 }
 func (u *UserServiceServerImpl) IncrUserCounter(ctx context.Context, req *hpb.IncrUserCounterMessage) (*hpb.IncrUserCounterResponse, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "incr_user")
+	defer span.Finish()
 	if err := db.IncrementUserReservationCount(req.UserId); err != nil {
 		return nil, grpc.Errorf(codes.Internal, "db.IncrementUserReservationCount failed:%v", err)
 	}
 	return &hpb.IncrUserCounterResponse{}, nil
 }
 func (u *UserServiceServerImpl) DecrUserCounter(ctx context.Context, req *hpb.DecrUserCounterMessage) (*hpb.DecrUserCounterResponse, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "decr_user")
+	defer span.Finish()
 	if err := db.DecrementUserReservationCount(req.UserId); err != nil {
 		return nil, grpc.Errorf(codes.Internal, "db.DecrementUserReservationCount failed:%v", err)
 	}
